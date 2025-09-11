11de05b50e3013c6c79390d150bd1fbf78fda73b
KoniwaZ2 <23502410024@student.prasetiyamulya.ac.id>
2025-09-11 20:21:21 +0700

    .

---
+++ Lab6-Victor/DjangoSetup/project1/aplikasi/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Mahasiswa(models.Model):
+  nim = models.CharField(max_length=10, unique=True, null=True)
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  jurusan = models.CharField(max_length=100,null=True)
+
+
+  def __str__(self):
+    return f"{self.nim} - {self.firstname} {self.lastname} - {self.jurusan}"

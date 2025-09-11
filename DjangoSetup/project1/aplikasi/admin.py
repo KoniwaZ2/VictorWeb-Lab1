19451906c61983f0cd0748886fdb9349129f3d3f
KoniwaZ2 <23502410024@student.prasetiyamulya.ac.id>
2025-09-11 20:22:58 +0700

    Push

--- DjangoSetup/project1/aplikasi/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Mahasiswa
-
-# Register your models here.
-
-class MahasiswaAdmin(admin.ModelAdmin):
-  list_display = ("nim", "firstname", "lastname", "jurusan",)
-
-admin.site.register(Mahasiswa, MahasiswaAdmin)
